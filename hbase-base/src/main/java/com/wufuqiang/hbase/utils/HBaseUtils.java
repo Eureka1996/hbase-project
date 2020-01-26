41522e917953fff8d4aea7dddbfda059887bf6c6
eureka_china <1767778344@qq.com>
2020-01-26 17:13:52 +0800

    init

---
+++ hbase-base/src/main/java/com/wufuqiang/hbase/utils/HBaseUtils.java
@@ -0,0 +1,22 @@
+package com.wufuqiang.hbase.utils;
+
+/**
+ * @ author wufuqiang
+ **/
+public class HBaseUtils {
+
+    private HBaseUtils(){
+
+    }
+
+    public static HBaseUtils getInstance(){
+
+        return new HBaseUtils();
+    }
+
+    public boolean isTableExist(String tableName){
+
+
+    }
+
+}
