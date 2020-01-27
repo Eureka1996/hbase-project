01c9153a7618814d89bffe6ccbc77b8a98bd6f00
eureka_china <1767778344@qq.com>
2020-01-27 10:28:48 +0800

    判断表是否存在

--- hbase-base/src/main/java/com/wufuqiang/hbase/utils/HBaseUtils.java
+++ hbase-base/src/main/java/com/wufuqiang/hbase/utils/HBaseUtils.java
@@ -1,22 +1,73 @@
 package com.wufuqiang.hbase.utils;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 /**
  * @ author wufuqiang
  **/
 public class HBaseUtils {
 
-    private HBaseUtils(){
+    private Configuration configuration ;
+    private Connection connection;
+    private Admin admin ;
+    private static Logger logger = LoggerFactory.getLogger(HBaseUtils.class);
+
+    private HBaseUtils(String zkQuorum) throws IOException {
+        configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum",zkQuorum);
+        connection = ConnectionFactory.createConnection(configuration);
+        admin = connection.getAdmin();
+    }
+
+    /**
+     * 获取HBaseUtils实例
+     * @param zkQuorum HBase地址
+     * @return
+     * @throws IOException
+     */
+    public static HBaseUtils getInstance(String zkQuorum) throws IOException {
+        return new HBaseUtils(zkQuorum);
+    }
+
+    /**
+     * 判断表是否存在
+     * @param tableName
+     * @return
+     * @throws IOException
+     */
+    public boolean isTableExist(String tableName) throws IOException {
+        return admin.tableExists(TableName.valueOf(tableName));
+    }
+
+    public void createTable(String tableName,String ... cfs) throws IOException {
+        if(cfs.length <= 0){
+            logger.error("没有列簇信息。");
+            return;
+        }
+        if(isTableExist(tableName)){
+            logger.info("HBase表已经存在。");
+            return;
+        }
 
     }
 
-    public static HBaseUtils getInstance(){
 
-        return new HBaseUtils();
-    }
-
-    public boolean isTableExist(String tableName){
-
-
+    public void close() throws IOException {
+        if(admin != null){
+            admin.close();
+        }
+        if(connection != null){
+            connection.close();
+        }
     }
 
 }
