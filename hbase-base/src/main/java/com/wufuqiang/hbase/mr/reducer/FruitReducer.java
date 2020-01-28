eaf575108eaa28cf24482ed2c2d4047f6d937fe0
eureka_china <1767778344@qq.com>
2020-01-28 19:08:50 +0800

    HBase MapReduce

--- hbase-base/src/main/java/com/wufuqiang/hbase/mr/reducer/FruitReducer.java
+++ hbase-base/src/main/java/com/wufuqiang/hbase/mr/reducer/FruitReducer.java
@@ -1,7 +1,19 @@
 package com.wufuqiang.hbase.mr.reducer;
 
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
 /**
  * @ author wufuqiang
  **/
-public class FruitReducer extends  {
+public class FruitReducer extends TableReducer<LongWritable,Text,NullWritable> {
+    @Override
+    protected void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+    }
 }
