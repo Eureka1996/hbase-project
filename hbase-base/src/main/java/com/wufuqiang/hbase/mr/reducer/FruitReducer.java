0aa95a7f9f44b6f23155b58aa02aa4b9d76cdd6a
eureka_china <1767778344@qq.com>
2020-01-29 17:01:02 +0800

    HBase MapReducer

--- hbase-base/src/main/java/com/wufuqiang/hbase/mr/reducer/FruitReducer.java
+++ hbase-base/src/main/java/com/wufuqiang/hbase/mr/reducer/FruitReducer.java
@@ -1,6 +1,8 @@
 package com.wufuqiang.hbase.mr.reducer;
 
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -14,6 +16,12 @@ import java.io.IOException;
 public class FruitReducer extends TableReducer<LongWritable,Text,NullWritable> {
     @Override
     protected void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        for (Text value : values) {
+            String[] fields = value.toString().split("\t");
+            Put put = new Put(Bytes.toBytes(fields[0]));
+            put.addColumn(Bytes.toBytes("info"),Bytes.toBytes("name"),Bytes.toBytes(fields[1]));
+            put.addColumn(Bytes.toBytes("info"),Bytes.toBytes("color"),Bytes.toBytes(fields[2]));
+            context.write(NullWritable.get(),put);
+        }
     }
 }
